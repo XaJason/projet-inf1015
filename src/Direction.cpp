67e402602787b0f4e4423516e3ac17a8ef07b67e
Merieme Bouisri <meriemebouisri@gmail.com>
2024-12-11 14:21:46 -0500

    FIX : cmakelist configuration

--- src/Direction.cpp
+++ src/Direction.cpp
@@ -3,7 +3,7 @@
 using namespace game_helper;
 
 namespace std {
-	template<> struct std::hash<Direction> {
+	template<> struct hash<Direction> {
 		size_t operator()(const Direction& direction) const noexcept {
 			return std::hash<int>{}(static_cast<int>((direction)));
 		}
