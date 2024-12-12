94202eb2ff82f7b02d88c131640de83acc0926f4
Merieme Bouisri <meriemebouisri@gmail.com>
2024-12-11 19:21:38 -0500

    ADD : Definition functions look, use, take + child Items

--- src/Item.cpp
+++ src/Item.cpp
@@ -2,6 +2,11 @@
 #include <iostream>
 
 namespace game_world {
+
+	std::ostream& Item::look(std::ostream& os) {
+		return os << this->getDescription();
+	}
+
 	std::ostream& operator<<(std::ostream& outputStream, const Item& item)
 	{
 		return outputStream << item.getName() << " : " << item.getDescription();
--- src/Item.h
+++ src/Item.h
@@ -1,6 +1,9 @@
 #pragma once
+#include "Player.h"
+#include "TileMap.h"
 #include <string>
 
+// TODO: Interface lookable (useable and takeable ?)
 namespace game_world {
 	/**
 	 * @class Item
@@ -26,6 +29,27 @@ namespace game_world {
 		 * @return La description de l'item 
 		 */
 		std::string getDescription() const { return description_; }
+
+		/**
+		 * @brief Methode permettant d'afficher la description d'un item
+		 * @param os Le flux de sortie
+		 */
+		std::ostream& look(std::ostream& os);
+
+		/**
+		 * @brief Methode permettant d'ajouter un item a l'inventaire d'un joueur
+		 * @param player La reference au Player qui prend l'item
+		 * @return true si l'item a ete ajoute a l'inventaire du joueur avec succes
+		 */
+		bool take(const game_world::Player& player);
+
+		/**
+		 * @brief Methode permettant d'utiliser un item. 
+		 * @param player Reference au joueur qui utilise l'item
+		 * @return true si l'item a ete utilise avec succes
+		 */
+		virtual bool use(const game_world::Player& player) = 0;
+
 		/**
 		 * @brief Surcharge de l'operateur d'affichage
 		 * @param outputStream flux de sortie
@@ -37,5 +61,31 @@ namespace game_world {
 	private:
 		std::string name_; ///< Nom associe a l'item
 		std::string description_; ///< Description de l'item
+	};
+
+	/**
+	 * @class KeyItem
+	 * @brief Item permettant d'acceder a de nouvelles salles.
+	 */
+	class KeyItem : public Item {
+		public:
+			KeyItem() : Item("Key", "Unlock new rooms") {};
+			bool use(const game_world::Player& player) override;
+	};
+
+	/**
+	 * @class CompassItem
+	 * @brief Item permettant de trouver la direction de la sortie 
+	 */
+	class CompassItem : public Item {
+		public:
+			CompassItem() : Item("Compass", "Find the direction of the exit from current position") {};
+			bool use(const game_world::Player& player) override;
+	};
+
+	class TrashItem : public Item {
+		public:
+			TrashItem() : Item("Trash", "A waste of space. Don't take it.") {};
+			bool use(const game_world::Player& player) override;
 	};
 }
